84050f6c2867f4049681d630866d4dc4abb465fd
kir345 <123649165+kir345@users.noreply.github.com>
2023-03-12 21:02:36 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,67 +1,58 @@
-﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с 
-//наименьшей суммой элементов.
-//Например, задан массив:
-//1 4 7 2
-//5 9 2 3
-//8 4 2 4
-//5 2 6 7
+﻿// ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+//Массив размером 2 x 2 x 2
+//66(0,0,0) 25(0,1,0)
+//34(1,0,0) 41(1,1,0)
+//27(0,0,1) 90(0,1,1)
+//26(1,0,1) 55(1,1,1)
 
 using System;
 using static System.Console;
 
-Clear();
 
 
+int[,,] array3D = new int[2, 2, 2];
+CreateArray(array3D);
+WriteArray(array3D);
 
-int[,] array = new int[4,4];
-CreateArray(array);
-WriteArray(array);
 
-int minSumLine = 0;
-int sumLine = SumLineElements(array, 0);
-for (int i = 1; i < array.GetLength(0); i++)
+void WriteArray(int[,,] array3D)
 {
-  int tempSumLine = SumLineElements(array, i);
-  if (sumLine > tempSumLine)
-  {
-    sumLine = tempSumLine;
-    minSumLine = i;
-  }
-}
-
-Console.WriteLine($"\n{minSumLine+1} - строкa с наименьшей суммой ({sumLine}) элементов ");
-
-
-int SumLineElements(int[,] array, int i)
-{
-  int sumLine = array[i,0];
-  for (int j = 1; j < array.GetLength(1); j++)
-  {
-    sumLine += array[i,j];
-  }
-  return sumLine;
-}
-
-
-void CreateArray(int[,] array)
-{
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
+    for (int i = 0; i < array3D.GetLength(0); i++)
     {
-      array[i, j] = new Random().Next(0,10);
+        for (int j = 0; j < array3D.GetLength(1); j++)
+        {
+            Write($"X({i}) Y({j}) ");
+            for (int k = 0; k < array3D.GetLength(2); k++)
+            {
+                Write($"Z({k}) = {array3D[i,j,k]}; ");
+            }
+            WriteLine();
+        }
+        WriteLine();
     }
-  }
 }
 
-void WriteArray (int[,] array)
+void CreateArray(int[,,] array3D)
 {
-  for (int i = 0; i < array.GetLength(0); i++)
-  {
-    for (int j = 0; j < array.GetLength(1); j++)
+    int[] temp = new int[array3D.GetLength(0) * array3D.GetLength(1) * array3D.GetLength(2)];
+    int num;
+    for (int i = 0; i < temp.GetLength(0); i++)
     {
-      Console.Write(array[i,j] + " ");
+        temp[i] = new Random().Next(10,100);
+        num = temp[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i] = new Random().Next(10,100);
+                    j = 0;
+                    num = temp[i];
+                }
+                    num = temp[i];
+            }
+        }
     }
-    Console.WriteLine();
-  }
 }
